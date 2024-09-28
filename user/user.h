31dc9d148682d1cbef1d7a926489c5213d99b5ea
ezcatcat <chenezcat@gmail.com>
2024-09-28 13:13:37 +0800

    lab2-trace

--- user/user.h
+++ user/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int trace(int); // 系统调用的原型
 
 // ulib.c
 int stat(const char*, struct stat*);
--- user/usys.pl
+++ user/usys.pl
@@ -36,3 +36,4 @@ entry("getpid");
 entry("sbrk");
 entry("sleep");
 entry("uptime");
+entry("trace"); # 添加存根
