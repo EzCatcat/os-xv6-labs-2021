2a39979fb1cc385f09d1aa97f419f4bba0234dca
ezcatcat <chenezcat@gmail.com>
2024-10-19 21:06:31 +0800

    lab3-打印进程1的页表

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void print_level(int level) {
+  switch (level)
+  {
+  case 0: printf(".."); break;
+  case 1: printf(".. .."); break;
+  case 3: printf(".. .. .."); break;
+  default: break;
+  }
+}
+
+void vmprint_process(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_W)) == 0) {
+      uint64 child = PTE2PA(pte);
+      print_level(level);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      vmprint_process((pagetable_t)child, level+1);
+    } else if(pte & PTE_V) {
+      uint64 pa = PTE2PA(pte);
+      print_level(level);
+      printf("%d: pte %p pa %p\n", i ,pte, pa);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_process(pagetable, 0);
+}
