2408d7c27cd3d47b383fd7825daf6326750e6b55
ezcatcat <chenezcat@gmail.com>
2024-11-09 20:55:14 +0800

    大文件

--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,10 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11 // 减少直接索引新增二级间接索引
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDINDIRECT NINDIRECT * NINDIRECT // 二级间接索引提供的块
+#define MAXFILE (NDIRECT + NINDIRECT + NDINDIRECT) // 修改总数据块的数量
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +36,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // Data block addresses
 };
 
 // Inodes per block.
